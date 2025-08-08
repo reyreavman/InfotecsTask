7b44c59b0819314182e384b5f2101159868bb77c
reyreavman <radmirkhurum@gmail.com>
2025-08-08 16:53:11 +0300

    InfotecsTT 028. Добавил таймауты

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -1,12 +1,14 @@
 package http
 
 import (
+	"context"
 	"errors"
 	"infotecstechtask/internal/facade"
 	"infotecstechtask/internal/models"
 	"infotecstechtask/internal/payment"
 	"infotecstechtask/internal/wallet"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -26,8 +28,10 @@ func NewHandler(facade facade.Facade) *Handler {
 
 func (h *Handler) CreateTransaction(c *gin.Context) {
 	createTransactionRequest := c.MustGet("validatedBody").(*models.CreateTransactionRequest)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	transaction, err := h.facade.CreateTransaction(c.Request.Context(), createTransactionRequest)
+	transaction, err := h.facade.CreateTransaction(ctx, createTransactionRequest)
 	if err != nil {
 		if errors.Is(err, payment.ErrSenderWalletNotFound) || errors.Is(err, payment.ErrRecipientWalletNotFound) || errors.Is(err, payment.ErrSenderAndRecipientSame) {
 			c.AbortWithStatusJSON(
@@ -36,6 +40,10 @@ func (h *Handler) CreateTransaction(c *gin.Context) {
 					Error: err.Error(),
 				},
 			)
+			return
+		}
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
 			return
 		}
 		c.AbortWithStatus(http.StatusInternalServerError)
@@ -47,12 +55,18 @@ func (h *Handler) CreateTransaction(c *gin.Context) {
 
 func (h *Handler) GetTransactions(c *gin.Context) {
 	params := c.MustGet("validatedParams").(*models.GetTransactionWithCountRequest)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	if params.Count != nil {
-		transactions, err := h.facade.GetTransactions(c.Request.Context(), *params.Count)
+		transactions, err := h.facade.GetTransactions(ctx, *params.Count)
 		if err != nil {
 			if errors.Is(err, wallet.ErrWalletNotFound) {
 				c.AbortWithStatus(http.StatusNotFound)
+				return
+			}
+			if errors.Is(err, context.DeadlineExceeded) {
+				c.AbortWithStatus(http.StatusServiceUnavailable)
 				return
 			}
 			c.AbortWithStatus(http.StatusInternalServerError)
@@ -61,10 +75,14 @@ func (h *Handler) GetTransactions(c *gin.Context) {
 
 		c.JSON(http.StatusOK, transactions)
 	} else {
-		transactions, err := h.facade.GetAllTransactions(c.Request.Context())
+		transactions, err := h.facade.GetAllTransactions(ctx)
 		if err != nil {
 			if errors.Is(err, wallet.ErrWalletNotFound) {
 				c.AbortWithStatus(http.StatusNotFound)
+				return
+			}
+			if errors.Is(err, context.DeadlineExceeded) {
+				c.AbortWithStatus(http.StatusServiceUnavailable)
 				return
 			}
 			c.AbortWithStatus(http.StatusInternalServerError)
@@ -77,11 +95,17 @@ func (h *Handler) GetTransactions(c *gin.Context) {
 
 func (h *Handler) GetWallet(c *gin.Context) {
 	walletId := uuid.MustParse(c.MustGet("validatedParams").(*models.GetWalletBalanceRequest).ID)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	walletToReturn, err := h.facade.GetWallet(c.Request.Context(), walletId)
+	walletToReturn, err := h.facade.GetWallet(ctx, walletId)
 	if err != nil {
 		if errors.Is(err, wallet.ErrWalletNotFound) {
 			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+		if errors.Is(err, context.DeadlineExceeded) {
+			c.AbortWithStatus(http.StatusServiceUnavailable)
 			return
 		}
 		c.AbortWithStatus(http.StatusInternalServerError)
